9f8bc52c80cf769550c6d80ee8d5171a919ccb8a
Abhishek Upadhyay <aupadhyay@onetrust.com>
2023-03-26 19:00:08 +0530

    Two Sum optimized solution

--- DataStructures/src/main/java/com/leet/code/arrays/TwoSum.java
+++ DataStructures/src/main/java/com/leet/code/arrays/TwoSum.java
@@ -1,17 +1,15 @@
 package com.leet.code.arrays;
 
-import jdk.jshell.execution.JdiInitiator;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
 
-        int[] nums = {2, 7, 11, 15};
-        System.out.println(Arrays.toString(twoSum(nums, 9)));
+        int[] nums = {3, 3};
+        System.out.println(Arrays.toString(twoSumV3(nums, 6)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -20,7 +18,7 @@ public class TwoSum {
             diff[i] = target - nums[i];
         }
         var diffList = new ArrayList<Integer>();
-        for(int d : diff){
+        for (int d : diff) {
             diffList.add(d);
         }
         var firstInd = -1;
@@ -33,5 +31,35 @@ public class TwoSum {
             }
         }
         return new int[]{firstInd, secondInd};
+    }
+
+    public static int[] twoSumV2(int[] nums, int target) {
+        Map<Integer, Integer> diffMap = new HashMap<>();
+        for (var i = 0; i < nums.length; i++) {
+            diffMap.put(target - nums[i], i);
+        }
+        int firstInd = -1;
+        int secondInd = -1;
+        for (var i = 0; i < nums.length; i++) {
+            if (diffMap.containsKey(nums[i]) && i != diffMap.get(nums[i])) {
+                firstInd = i;
+                secondInd = diffMap.get(nums[i]);
+                break;
+            }
+        }
+        return new int[]{firstInd, secondInd};
+    }
+
+    public static int[] twoSumV3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
     }
 }
